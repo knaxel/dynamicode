06d05dd11649143cd10dae316714a341142ec20b
MattEMoran <mamo2629@colorado.edu>
2021-08-14 12:01:00 -0600

    Redid module 8

--- codigram/modules/python/python_8.py
+++ codigram/modules/python/python_8.py
@@ -5,33 +5,33 @@ MODULE_ID = "python_8"
 MODULE_DATA = {
     "title": "Module 8: I/O",
     "author": "Codigram",
-    "author_uuid": "3473989f-0e1f-4488-9e98-847da1969d41",
-    "sandbox_uuid": "ea536da3-84bf-4b07-a34b-43befbff5ce8",
-    "date_created": "Aug 05, 2021 04:15 PM",
+    "author_uuid": "",
+    "codepage_uuid": "7c862055-115f-4858-ac68-e6892dc18473",
+    "date_created": "Aug 14, 2021 11:51 AM",
     "blocks": [
         {
-            "name": "Input Function",
-            "text": "We’ve been using print() to output useful information, but this is only half of I/O - Input/Output. You may want your program to take input from a user and then print relevant information based on this input. You can use the input() function as shown to allow users to type anything as input:    \n     \n        x = input()        \n        \nIf you want to add a prompt, you can add text in-between the parentheses:        \n        \n        x = input(‘What’s your name? Type here: ’)        \n",
+            "name": "The Input Function",
+            "text": "We’ve been using `print()` to output useful information, but this is only half of I/O, which stands for Input/Output. You may want your program to take input from a user and then print relevant information based on this input. You can use the `input()` function as shown to allow users to type strings as input:\n```    \nx = input()     \n```\nIf you want to add a prompt, you can add text in-between the parentheses:    \n```    \nx = input(‘What’s your name? Type here: ’)      \n```\nA user’s input will always be treated as a string unless you type cast it to a different data type.",
             "type": "TextBlock"
         },
         {
             "name": "String Formatting",
-            "text": "A user’s input will often need to show up in some way in output. If the input was a string, we can also concatenate it to other strings in a print statement like shown:       \n       \n       name = input()       \n       print(“Hi “ + name)       \n       \nThere are much fancier ways of doing this that are useful for non-string values as well. Take a look at the following code.       \n       \n       x = “World!”       \n       print(f ‘Hello {x}’)       \n       \nLet’s break this down. We start by declaring a string variable called x whose value is “World!” We then have a print statement which holds a string preceded by the character ‘f’. The f character is used to make the string inside the print statement a formatted string literal. By doing so, we can place variable names inside brackets to easily add them to the print statement, as shown above.\nNow take a look at this block of code.       \n       \n       name = “George”       \n       print(“Hi, my name is {0}”.format(name))       \n       \nWe can use the format method to insert values into strings just like with the formatted string variables. Instead of putting the variable name in brackets, you put the index of the entry in the format method. For example, if we had `.format(name, age)` instead, we would use {0} to insert the name and {1} to insert the age.",
+            "text": "A user’s input will often need to show up in some way in output. Since the input is a string by default, we can concatenate it with other strings in a print statement like shown:     \n```     \nname = input()     \nprint(“Hi ” + name)     \n```\nThere are much fancier ways of doing this that are useful for non-string values as well. Take a look at the following code.    \n```    \nx = “World!”     \nprint(f ‘Hello {x}’)    \n```\nLet’s break this down. We start by declaring a string variable called `x` which has a value of “World!” We then have a print statement which holds a string preceded by the character ‘f’. The f character is used to make the string inside the print statement a formatted string literal. By doing so, we can place variable names inside brackets to easily add them to the print statement, as shown above.\nNow take a look at this block of code.     \n```    \nname = “George”     \nprint(“Hi, my name is {0}”.format(name))     \n```\nWe can use the `format` method to insert values into strings just like before. Instead of putting the variable name in brackets, you put the index of the argument that is in the format method. For example, if we had .format(name, age) instead, we would use {0} to insert the name and {1} to insert the age.",
             "type": "TextBlock"
         },
         {
-            "name": "Madlibs Activity",
+            "name": "Madlibs",
             "text": "We can make strings that are very dependent on user input with very little effort. The following program can be completed with just a single line of code! Fill out the format method so that all 6 values are determined by user input. Add prompts so that each value the user inputs is only a specific type of word - the first and fourth inputs should be adjectives, the second and fifth inputs should be colors, and the third and sixth inputs should be nouns. Then try it out for yourself!",
             "type": "TextBlock"
         },
         {
-            "name": "Madlibs Activity ",
+            "name": "Madlibs Activity",
             "code": "print('The {0} {1} {2} jumped over the {3} {4} {5}'.format())",
             "type": "CodeBlock"
         },
         {
-            "name": "Check for Understanding 1",
-            "text": "What output will the following code produce?\n\n\ta = “b”\n\tb = “c”\n\tc = “d”\n\n\tprint(‘{1}{2}{0}{2}’.format(c,b,d,a))",
+            "name": "Check for Understanding",
+            "text": "What output will the following code produce?    \n```    \n\ta = “b”     \n\tb = “c”     \n\tc = “d”     \n     \n\tprint(‘{1}{2}{0}{2}’.format(c,b,d,a))     \n```",
             "choices": [
                 "cdba",
                 "bbcd",
@@ -42,8 +42,8 @@ MODULE_DATA = {
             "type": "ChoiceBlock"
         },
         {
-            "name": "Check for Understanding 2",
-            "text": "What output will the following code produce?\n\n\ta = “b”\n\tb = “c”\n\tc = “d”\n\n\tprint(‘{1}{2}{0}{2}’.format(c,a,a))",
+            "name": "Check for Understanding ",
+            "text": "What output will the following code produce?    \n```    \n\ta = “b”    \n\tb = “c”    \n\tc = “d”    \n     \n\tprint(‘{1}{2}{0}{2}’.format(c,a,a))     \n```",
             "choices": [
                 "ccdc",
                 "bbdb",

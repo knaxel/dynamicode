383d207150e34599ff7b3ad447fdab10a904059b
MattEMoran <mamo2629@colorado.edu>
2021-08-14 17:29:10 -0600

    Redid modules 9 and 10

--- codigram/modules/python/python_10.py
+++ codigram/modules/python/python_10.py
@@ -5,39 +5,61 @@ MODULE_ID = "python_10"
 MODULE_DATA = {
     "title": "Module 10: Functions",
     "author": "Codigram",
-    "author_uuid": "3473989f-0e1f-4488-9e98-847da1969d41",
-    "sandbox_uuid": "13ffa4cf-8054-4542-938b-626c8d1f21e2",
-    "date_created": "Aug 05, 2021 05:18 PM",
+    "author_uuid": "",
+    "codepage_uuid": "2dabb8d7-0eb2-43b8-9d90-8b4869caaa8c",
+    "date_created": "Aug 14, 2021 05:10 PM",
     "blocks": [
         {
             "name": "What is a Function?",
-            "text": "A function is a block of code that will run when it is called from somewhere else in a program. An often utilized function in Python is the main function. When coupled with a special if statement, the main function will always be the first function to run in a python file.    \n      \n        def main():        \n                pass        \n        \n        if __name__ == \"__main__\":\n                main()\n        \nFunctions can make code much more readable and concise by taking commonly used blocks of code and condensing them down to one line function calls. You have already used a number of functions throughout these modules - `print()`, `sort()`,`len()`, etc. We called these functions methods because they are functions associated with classes and objects, but we will go over this more in a future module.",
+            "text": "A function is a block of code that will run when a call is made to it from somewhere else in a program. An often utilized function in Python is the main function. When coupled with a special if statement, the main function will always be the first function to run in a python file.    \n```     \ndef main():    \n\t...     \n     \nif __name__ == \"__main__\":     \n   \tmain()     \n```\nFunctions can make code much more readable and concise by taking commonly used blocks of code and condensing them down to one line function calls. You have already used a number of functions throughout these modules - `print()`, `sort()`, `len()`, etc. We called these functions methods because they are functions associated with classes and objects, but we will go over this more in a future module.",
             "type": "TextBlock"
         },
         {
             "name": "Defining Functions",
-            "text": "To define a function, the def keyword is used, followed by the function name and parentheses. Any parameters that will provide the function with input go into the parentheses as shown below.    \n    \n        def function_name():        \n                print(“This is a function”)        \n        \n        def function2_name(parameter_name):        \n\t        print(parameter)        \n        \nTo call these functions, just use the function names.        \n        \n        function_name()        \n        function2_name(parameter_value)        \n        \nIt is important here to remember how scope can affect your code. If you were to call a custom function and try to access a variable created inside the main function, you would be unable to do so because that variable would not be in the scope of your function. An example is below.        \n        \n        def function_name():        \n\t        x = x + 1 # This would not work!        \n        \n        def main():        \n\t        x = 0\n\t        function_name()\n        \nHowever, if you were to define a variable outside of both functions (a global variable), your function could access it since now it is in scope.        \n        \n        y = 0        \n        \n        def function_name():        \n\t        print(y) # This would work!        \n        \n        function_name()        ",
+            "text": "To define a function, the `def` keyword is used, followed by the function name and parentheses. Any parameters that will provide the function with input go into the parentheses as shown below.     \n```    \ndef function_name():    \n\tprint(“This is a function”)     \n     \ndef function2_name(parameter_name):    \n\tprint(parameter)     \n```\nTo call these functions, just use the function names.     \n```     \nfunction_name()      \nfunction2_name(parameter_value)      \n```\nIt is important here to remember how scope can affect your code. If you were to call a custom function and try to access a variable created inside the main function, you would be unable to do so because that variable would not be in the scope of your custom function. An example is below.     \n```     \ndef function_name():     \n\tx = x + 1 # This would not work! x is not defined in the scope of function_name    \n     \ndef main():    \n\tx = 0     \n\tfunction_name()      \n```\nHowever, if you were to define a variable outside of both functions (a global variable), your function could access it since now it is in scope.    \n```    \ny = 0    \n     \ndef function_name():     \n\tprint(y) # This would work!      \n      \nfunction_name()      \n```\nYou can also pass values, called arguments, to a function, whether or not they are in scope.",
             "type": "TextBlock"
         },
         {
             "name": "Parameters and Arguments",
-            "text": "Parameters give the advantage of being able to pass values (that may or may not be in scope) to functions without necessarily changing them. When we used len(), we passed in a string as input like this: len(string_name). Doing so effectively created a copy of the string for the method to use. To define the parameters of a function, we put the names that we want to call them inside the parentheses of the function declaration.     \n     \n        def name(parameter1, parameter2):        \n                pass        \n        \nWe then pass values to these parameters in the function call.        \n        \n        name(“value1”,”value2”)        \n        \nYou cannot pass more or less values than there are parameters. So in the case above, you must always pass two parameters. If you wish to add default parameters values in the case the function is called without arguments, you can add them by setting the parameter names equal to a value.        \n        \n        def name(parameter=”default”):        \n                pass        ",
+            "text": "Parameters and arguments give the advantage of being able to pass values (that may or may not be in scope) to functions without necessarily changing them. When we used `len()`, we passed in a string as input like this: `len(string_name)`. Doing so effectively created a copy of the string for the method to use. To define the parameters of a function, we put the names that we want to call them inside the parentheses of the function declaration.     \n```      \ndef name(parameter1, parameter2):    \n\t...    \n```\nWe then pass values to these parameters in the function call.     \n```    \nname(“value1”,”value2”)     \n```\nYou cannot pass more or less arguments than there are parameters. So in the case above, you must always pass two arguments. If you wish to add default parameters values in the case the function is called without arguments, you can add them by setting the parameter names equal to a value.    \n```     \ndef name(parameter=”default”):    \n\t...     \n```",
             "type": "TextBlock"
         },
         {
-            "name": "The Pass and Return Keyword",
-            "text": "Finally, let’s discuss two of Python’s many keywords: pass and return. Pass is used whenever you don’t want to execute code. For example, if you want to declare an empty function but you haven’t written any code for it, you can simply put “pass” on a new line in the function. This can be used to prevent any errors from declaring an empty function.      \n      \nReturn is used to “return” a value from a function to the original caller of that function. The example below shows how we can set a variable equal to a function call, and that function call can return a value that becomes the value of the variable. Once the line with return is run, the function will terminate - no lines of code after a return keyword will run, assuming they’re in the same block of code.    \n     \n        def name():        \n                return 2        \n        \n        x = name()        \n        print(x) # Prints 2        ",
+            "name": "The Pass and Return Keywords",
+            "text": "Finally, let’s discuss two of Python’s many keywords: `pass` and `return`. `pass` is used whenever you don’t want to execute code. For example, if you want to declare an empty function but you haven’t written any code for it, you can simply put `pass` on a new line in the function. This can be used to prevent any errors from declaring an empty function.        \n```     \ndef name():    \n\tpass     \n ```\n`return` is used to “return” a value from a function to the original caller of that function. The example below shows how we can set a variable equal to a function call, and that function call can return a value that becomes the value of the variable. Once the line with return is run, the function will terminate - no lines of code after a return keyword will run, assuming they’re in the same block of code.\n```   \ndef name():    \n\treturn 2     \n    \nx = name()     \nprint(x) # Prints 2    \n```",
+            "type": "TextBlock"
+        },
+        {
+            "name": "Recursion",
+            "text": "You may or may not have heard the term recursion before. In the field of computer science, recursive problems are problems that are solved by looking at smaller but identical problems and solving those. A common example of recursion is what is called a factorial. In math, if you have a number followed by an exclamation mark, that is the factorial of that number. Its value is itself multiplied by itself minus 1, and then multiplied by that minus 1, and so on until 1. For example, 4! is 4 times 3 times 2 times 1 (4 * 3 * 2 * 1), or 24.      \nYou may find it easier to understand recursion by writing recursive functions. Take a look at the function below.     \n```   \ndef function(x):     \n\tif(x==1):     \n\t\tprint(1)     \n\telse:     \n\t\tfunction(x-1)      \n```\nNotice how we call the function within itself when `x` is not equal to 1. Assuming we pass a positive integer to the function as an argument, the function will call itself using the argument minus 1 until the argument is actually equal to 1, at which point it will terminate. So if we originally called the function with an argument of 3, `function(x-1)` would be called, passing an argument of 2 to the function. If we provide a negative value (or zero) as an argument, this function would never terminate because the arguments being passed to the function would simply decrease to negative infinity and never reach 1.     \n       \nYour task will be to use a recursive function to calculate the factorial of a number as we described above. Remember that you should use conditional statements so that the function terminates at a certain point. The skeleton code is provided for you below.     ",
             "type": "TextBlock"
         },
         {
             "name": "Recursion Activity",
-            "text": "You may or may not have heard the term recursion before. In the field of computer science, recursive problems are problems that are solved by solving smaller but identical problems. A common example of recursion is what is called a factorial. In math, if you have a number followed by an exclamation mark, that is the factorial of that number, and its value is the first value multiplied by itself minus 1, and then multiplied by that minus 1, and so on until 1. For example, 4! is 4 times 3 times 2 times 1, or 24.         \n        \nYou may find it easier to understand recursion by writing recursive functions. Take a look at the function below.      \n\n        def function(x):        \n        if(x==1):        \n                print(1)        \n        else:        \n                function(x-1)        \n        \nNotice how we call the function within itself when x is not equal to 1. Assuming we pass a positive value to the function, the function will call itself using the argument minus 1 until the argument is 1, where it will terminate. If we gave a negative value (or zero), this function would never terminate because the arguments being passed to the function would simply decrease to negative infinity and never reach 1.        \n        \nYour task will be to use a recursive function to calculate the factorial of a number as we described above. Remember that you should use conditional statements so that the function terminates at a certain point. The skeleton code is provided for you below.        \n",
-            "type": "TextBlock"
+            "code": "def factorial(inputNumber):\n\tpass ",
+            "type": "CodeBlock"
         },
         {
-            "name": "A",
-            "code": "def factorial(inputNumber):\n\tpass \n  \n  \n  ",
-            "type": "CodeBlock"
+            "name": "Check for Understanding 1",
+            "text": "What is wrong with the following code?\n```   \ndef mysteryFunc(y):    \n\tprint(“Input: “ + (x+y))    \n    \nx = 2    \nmysteryFunc(5)      \n```",
+            "choices": [
+                "x is not in scope in mysteryFunc",
+                "x is declared after it is used in mysteryFunc",
+                "x is not a string",
+                "There is nothing wrong with the code"
+            ],
+            "type": "ChoiceBlock"
+        },
+        {
+            "name": "Check for Understanding 2",
+            "text": "What is wrong with the following code?\n```    \ndef mysteryFunc(z):    \n\tprint(“Input: “ + (x+z))     \n      \nx = 2    \nmysteryFunc(x)      \n```",
+            "choices": [
+                "x is not in scope in mysteryFunc",
+                "x is declared after it is used in mysteryFunc",
+                "x is not a string",
+                "There is nothing wrong with the code"
+            ],
+            "type": "ChoiceBlock"
         }
     ]
 }
--- codigram/modules/python/python_9.py
+++ codigram/modules/python/python_9.py
@@ -4,40 +4,45 @@ MODULE_ID = "python_9"
 
 MODULE_DATA = {
     "title": "Module 9: Lists",
-    "author": "Matt01",
-    "author_uuid": "3473989f-0e1f-4488-9e98-847da1969d41",
-    "sandbox_uuid": "3f23b12c-df8c-49e9-93e4-bdc3b6809173",
-    "date_created": "Aug 05, 2021 04:38 PM",
+    "author": "Codigram",
+    "author_uuid": "",
+    "codepage_uuid": "9e19eaa7-9da4-4f85-aac4-6d340580bf5b",
+    "date_created": "Aug 14, 2021 04:11 PM",
     "blocks": [
         {
             "name": "Declaring Lists",
-            "text": "It’s often preferred to store related values together in a single variable. In Python, this is called a list. In other languages, it is often called an array or a vector. A list can store multiple values of any variable type, including other lists. Each value is referred to as an item (or an element in other languages). Lists can be declared by setting a variable name equal to items separated by commas within brackets, as shown.    \n     \n        list = [“value1”,”value2”,”value3”]        ",
+            "text": "It’s often preferred to store related values together in coding to make it easier to access them and use them at the same time. In Python, we can use what is called a list. In other languages, it is often called an array or a vector. A list can store multiple values of any variable type, including other lists. Each value is referred to as an item (or an element in other languages). Lists can be declared by setting a variable name equal to items separated by commas within brackets, as shown.\n```    \nlist = [“value1”,”value2”,”value3”]     \n```",
             "type": "TextBlock"
         },
         {
-            "name": "Accessing Items",
-            "text": "Recall that we could look at individual characters in a string by using brackets and the position of the character in the string, starting from 0. To access an item in a list, you can use list_name[index], where index starts at 0 for the first item.     \n     \n        list=[1,3,5,7,9]        \n        \nSo `list[1]` would return 3, and `list[0]` would return 1.",
+            "name": "Accessing Elements",
+            "text": "Recall that we could look at individual characters in a string by using brackets and the position of the character in the string, starting from 0. To access an item in a list, you can use `list_name[index]`, where index always starts at 0 for the first item.     \n```       \nlist=[1,3,5,7,9]      \n```\nSo `list[1]` would return 3, and `list[0]` would return 1.",
             "type": "TextBlock"
         },
         {
             "name": "Looping Through Lists",
-            "text": "Also recall that there were multiple ways to iterate over all the characters in a string using a for loop. We can iterate over all the items in a list using the same two methods, as demonstrated below.   \n    \n        list = [1,2,3]        \n        for i in range(0,3):        \n                print(list[i])        \n        \n        list2 = [1,2,3]        \n        for x in list2:        \n\t        print(x)        ",
+            "text": "Also recall that there were multiple ways to iterate over all the characters in a string using a for loop. We can iterate over all the items in a list using the same two methods, as demonstrated below.    \n```    \nlist = [1,2,3]     \nfor i in range(0,3):     \n\tprint(list[i])     \n```\n```    \nlist2 = [1,2,3]    \nfor item in list2:     \n\tprint(item)      \n```",
             "type": "TextBlock"
         },
         {
             "name": "Adding and Deleting Items",
-            "text": "There are various ways to add and remove items from lists. The first is the append method, which adds an item to the end of a list.     \n     \n        list = [1,2]        \n        list.append(3)        \n        # list = [1,2,3]        \n        \nThe second is the insert method, which lets you insert an item at a specific index. It pushes all items at and after that index over 1, so using the insert method does not replace items.        \n        \n        list=[1,3]        \n        list.insert(1,2)        \n        \nThe first parameter is the index at which the item will be inserted (1), and the value that is being inserted is the second parameter (2).        \nYou can append one list to another using the extend method as shown.        \n        \n        list = [1,2,3]        \n        list2 = [4,5,6]        \n        list.extend(list2)        \n        # print(list) will give [1,2,3,4,5,6]        \n        \nThere are also multiple methods for removing items. The remove method lets you choose exactly which item to remove.        \n        \n        list=[‘a’,’b’,’c’]        \n        list.remove(‘a’)        \n        # list is now [‘b’,’c’]        \n        \nThe pop method lets you choose the index of the item to remove.        \n        \n        list=[‘a’,’b’,’c’]        \n        list.pop(2)        \n        # list is now [‘a’,’b’]        \n        \nUsing pop() without an index just removes the last item in the list.\nYou can also use the del keyword to delete items at particular indices.        \n        \n        list=[‘a’,’b’,’c’]        \n        del list[0]        \n        # list is now [‘b’,’c’]        \n\nFinally, you can use the clear method to remove all items from a list. This does not delete the list, so you can continue to add and remove items from it.        \n        \n        list=[‘a’,’b’,’c’]        \n        list.clear()        \n        # list is now [ ]        ",
+            "text": "There are various ways to add and remove items from lists. The first is the `append` method, which adds an item to the end of a list.    \n```    \nlist = [1,2]     \nlist.append(3)      \n# list = [1,2,3]     \n```\nThe second is the `insert` method, which lets you insert an item at a specific index. It pushes all items at and after that index over 1, so using the insert method does not replace items.       \n```     \nlist=[1,3]     \nlist.insert(1,2)     \n# list = [1,2,3]     \n```\nThe first parameter is the index at which the item will be inserted (1), and the value that is being inserted is the second parameter (2).    \nYou can append one list to another using the `extend` method as shown.      \n```     \nlist = [1,2,3]      \nlist2 = [4,5,6]     \nlist.extend(list2)     \n# print(list) will give [1,2,3,4,5,6]      \n```\nThere are also multiple methods for removing items. The `remove` method lets you choose exactly which item to remove.      \n```     \nlist=[‘a’,’b’,’c’]     \nlist.remove(‘a’)     \n# list is now [‘b’,’c’]     \n```\nThe `pop` method lets you choose the index of the item to remove.    \n```    \nlist=[‘a’,’b’,’c’]     \nlist.pop(2)    \n# list is now [‘a’,’b’]     \n```\nUsing `pop()` without an index just removes the last item in the list.    \nYou can also use the `del` keyword to delete items at particular indices.      \n```     \nlist=[‘a’,’b’,’c’]    \ndel list[0]    \n# list is now [‘b’,’c’]     \n```\nFinally, you can use the `clear` method to remove all items from a list. This does not delete the list, so you can continue to add and remove items from it.     \n```     \nlist=[‘a’,’b’,’c’]    \nlist.clear()    \n# list is now [ ]     \n```",
             "type": "TextBlock"
         },
         {
             "name": "Sorting",
-            "text": "Python has its own method for sorting lists in ascending or descending order, if applicable.     \n     \n        list=[‘a’,’d’,’b’]        \n        list.sort()        \n        # The list is now sorted in alphabetical order.        \n        \n        list2=[5,2,9,-10]        \n        list2.sort(reverse=True)        \n        # The list is now sorted in descending order.        \n        \nThe sort method also can take in a special parameter that lets you sort based on a function that you define. You will learn more about functions in the next module, but below is an example for reference.        \n        \n        # A function that returns whether or not the value is less than 10:        \n        def compFunc(e):        \n                return e < 10        \n        \n        list = [12, 3, 2, 17]        \n        list.sort(key=compFunc)        \n        \n# print(list) will output [12,17,3,2] because the list was sorted into two groups - numbers less than 10, and numbers greater than or equal to 10.        ",
+            "text": "Python has its own method for sorting lists in ascending or descending order, if applicable.      \n```    \nlist=[‘a’,’d’,’b’]     \nlist.sort()      \n# The list is now sorted in alphabetical order.     \n    \nlist2=[5,2,9,-10]     \nlist2.sort(reverse=True)      \n# The list is now sorted in descending order.     \n```\nThe `sort` method also can take in a special parameter that lets you sort based on a function that you define. You will learn more about functions in the next module, but below is an example for reference.    \n```   \n# A function that returns whether or not the value is less than 10:     \ndef compFunc(e):    \n        return e < 10     \n     \nlist = [12, 3, 2, 17]    \n     \nlist.sort(key=compFunc)    \n```\nPrinting the list will output [12,17,3,2] because the list was sorted into two groups - numbers less than 10, and numbers greater than or equal to 10.      ",
             "type": "TextBlock"
         },
         {
-            "name": "Strings are Lists",
-            "text": "Recall that we can access characters of a string using string_name[index]. Notice how this is identical to how we access items in a list. This is because strings are actually arrays (lists) of characters. So the length method works on both strings and lists.",
+            "name": "Swiss Cheese",
+            "text": "If we want to actually manipulate the characters of a string, we can turn the string into a list, modify the items, and then turn the list back into a string. Below is some skeleton code that you should complete so that all of the outputted strings are correct.",
             "type": "TextBlock"
+        },
+        {
+            "name": "Swiss Cheese Activity",
+            "code": "def addHoles(originalString):\n\t# First, convert the string into a list using an add function of your choice.\n\t# Add a for loop that replaces each “a” character in the list with a “.“ (space) character in order to add the \"holes\".\n\t# Call the sort function on the list to sort each character alphabetically.\n\t# Convert the list back into a string and print it\n\naddHoles(\"abcdefg\")\naddHoles(\"03keodle\")\naddHoles(\"..cad\")",
+            "type": "CodeBlock"
         },
         {
             "name": "Check for Understanding 1",
@@ -54,26 +59,26 @@ MODULE_DATA = {
         },
         {
             "name": "Check for Understanding 2",
-            "text": "What method will remove the item at index 3 in a 4 element list?",
+            "text": "What method will remove the item at index 3 in a 4 item list?",
             "choices": [
                 "list.pop(2)",
                 "list.pop(3)",
                 "list.pop()",
                 "Both a and b",
                 "Both b and c",
-                "All of the above"
+                "Both a, b, and c"
             ],
             "type": "ChoiceBlock"
         },
         {
             "name": "Check for Understanding 3",
-            "text": "What is the output of the following code?\n\n        list = [‘g’,’d’,’e’,’t’,’r’]        \n        for i in range(0,3):        \n\t        print(list[len(list)-i-1])        ",
+            "text": "What is the output of the following code?\n```   \nlist = [‘g’,’d’,’e’,’t’,’r’]     \nfor counter in range(0,3):     \n\tprint(list[len(list)-counter-1])     \n```",
             "choices": [
-                "r t e",
-                "g d e",
-                "t e d",
-                "e t r ",
-                "d e t"
+                "rte",
+                "gde",
+                "ted",
+                "etr",
+                "det"
             ],
             "type": "ChoiceBlock"
         }
